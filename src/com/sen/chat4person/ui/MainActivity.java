6d4da38683b9100361cf908f49ea429d9373ed30
nice70 <852555148@qq.com>
2014-10-15 21:25:43 +0800

    chat4person project demo

---
+++ src/com/sen/chat4person/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sen.chat4person.ui;
+
+import com.sen.chat4person.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
